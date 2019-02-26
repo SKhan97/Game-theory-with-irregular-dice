5b37e09f5b9afd1d7e60876f2c5e559e08a4f998
SKhan97 <khan_s97@hotmail.co.uk>
2019-02-26 17:48:46 +0000

    Winrates can now be calculated against the other dice

--- Dice.py
+++ Dice.py
@@ -1,25 +1,46 @@
 import random
+from tqdm import trange
 
-dice1_values = [3, 5/6, 6, 1/6, 'Transparent'] #Transparent die
-dice2_values = [2, 1/2, 5, 1/2, 'Grey'] #Grey die
-dice3_values = [1, 1/6, 4, 5/6, 'Blue'] #Blue die
+dice1_dict = {
+    'name': 'Transparent',
+    'value1': 3,
+    'prob1': 5/6,
+    'value2': 6,
+    'prob2': 1/6,
+}
 
+dice2_dict = {
+    'name': 'Grey',
+    'value1': 2,
+    'prob1': 1/2,
+    'value2': 5,
+    'prob2': 1/2,
+}
+
+dice3_dict = {
+    'name': 'Blue',
+    'value1': 1,
+    'prob1': 1/6,
+    'value2': 4,
+    'prob2': 5/6,
+}
 
 class Dice():
     "Class to generate a die which can only return 2 values"
-    def __init__(self, dice_array):
-        self.value1 = dice_array[0]
-        self.prob1 = dice_array[1]
-        self.value2 = dice_array[2]
-        self.prob2 = dice_array[3]
-        self.name = dice_array[4]
+    def __init__(self, dice):
+        self.value1 = dice['value1']
+        self.prob1 = dice['prob1']
+        self.value2 = dice['value2']
+        self.prob2 = dice['prob2']
+        self.name = dice['name']
 
     def roll(self):
         x=random.random()
+
         if x <= self.prob1:
             return self.value1
-        elif self.prob1 < x <= 1:
-            return self.value2
+
+        return self.value2
 
 
     def compare(self, dice2):
@@ -28,39 +49,68 @@ class Dice():
 
         if value1>value2:
             return self.name
-            #print("%s won with a value of %i compared to %i"% (self.name, value1,value2))
         elif value2>value1:
             return dice2.name
-            #print("%s won with a value of %i compared to %i"% (dice2.name, value2, value1))
         else:
-            print('WTF')
+            raise Exception('Error with values')
 
     def calculate(self, dice2, n):
        dice1_wins = 0
        dice2_wins = 0
 
-       for i in range(n): #Need to incorparate the i somehow
-           if self.compare(dice2) == self.name:
+       for i in trange(n):
+           winner = self.compare(dice2)
+           if winner == self.name:
                dice1_wins +=1
-               print(self.compare(dice2))
-           elif self.compare(dice2) == dice2.name:
+           elif winner == dice2.name:
                dice2_wins += 1
            else:
-               print(i)
+               print('#####')
 
-       print(dice1_wins, dice2_wins)
-
-
+       return(dice1_wins, dice2_wins)
 
 
+    def compete(self, other_dice, n):
+        dice_beaten = {}
+        dice_lost = {}
+        winrate = 0
+        for die in other_dice:
+            self_wins, other_wins = self.calculate(die,n)
+            win_percentage = 100 * self_wins / n
+
+            if self_wins > other_wins:
+                dice_beaten[die.name] = win_percentage
+            elif self_wins < other_wins:
+                dice_lost[die.name] = win_percentage
+            else:
+                raise Exception('Either the dice are the same or unseen error. Try rerunning script')
+
+        for key in dice_beaten:
+            winrate += dice_beaten[key]
+            print("%s beats %s with an average of %.1f%% win rate" % (self.name, key, dice_beaten[key]))
+
+        for key in dice_lost:
+            winrate += dice_lost[key]
+            print("%s loses to %s with an average of only %.1f%% win rate" % (self.name, key, dice_lost[key]))
+
+        average_winrate = winrate/len(other_dice)
+        print("%s has an average winrate of %.1f%% winrate against the other dice" % (self.name, average_winrate))
 
 
 
 
-Dice1 = Dice(dice1_values)
-Dice2 = Dice(dice2_values)
-Dice3 = Dice(dice3_values)
 
-Dice1.calculate(Dice2, 10)
+
+
+
+
+
+Dice1 = Dice(dice1_dict)
+Dice2 = Dice(dice2_dict)
+Dice3 = Dice(dice3_dict)
+
+Dice1.compete([Dice2, Dice3], 1000000)
+# Dice2.compete([Dice1, Dice3], 1000000)
+# Dice3.compete([Dice1, Dice2], 1000000)
 
 
